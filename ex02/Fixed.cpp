0ea4b747357fe4daa1e85a5359e45d962c0fe1d7
Mykola Movchan <mykolamovchan3721@gmail.com>
2025-08-23 11:07:41 +0200

    n

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -1,6 +1,6 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed(void){
+Fixed::Fixed(void) : _value(0){
 	std::cout << "Default constructor called" << std::endl;
 }
 
@@ -53,4 +53,82 @@ int Fixed::toInt(void) const{
 std::ostream &operator<<(std::ostream &out, const Fixed &fixed){
 	out << fixed.toFloat();
 	return out;
+}
+
+bool Fixed::operator<(const Fixed &other) const{
+	return this->_value < other._value;
+}
+
+bool Fixed::operator>(const Fixed &other) const{
+	return this->_value > other._value;
+}
+
+bool Fixed::operator<=(const Fixed &other) const{
+	return this->_value <= other._value;
+}
+
+bool Fixed::operator>=(const Fixed &other) const{
+	return this->_value >= other._value;
+}
+
+int Fixed::operator+(const Fixed &other) const{
+	return this->_value + other._value;
+}
+
+int Fixed::operator-(const Fixed &other) const{
+	return this->_value - other._value;
+}
+
+bool Fixed::operator==(const Fixed &other) const{
+	return this->_value == other._value;
+}
+
+int Fixed::operator*(const Fixed &other) const{
+	return this->_value * other._value;
+}
+
+int Fixed::operator/(const Fixed &other) const{
+	return this->_value / other._value;
+}
+
+bool Fixed::operator!=(const Fixed &other) const{
+	return this->_value != other._value;
+}
+
+Fixed &Fixed::operator++(){
+	this->_value++;
+	return *this;
+}
+
+Fixed Fixed::operator++(int){
+	Fixed tmp(*this);
+	this->_value++;
+	return tmp;
+}
+
+Fixed &Fixed::operator--(){
+	this->_value--;
+	return *this;
+}
+
+Fixed Fixed::operator--(int){
+	Fixed tmp(*this);
+	this->_value--;
+	return tmp;
+}
+
+Fixed& Fixed::min(Fixed &a, Fixed &b) {
+    return (a < b ? a : b);
+}
+
+const Fixed& Fixed::min(const Fixed &a, const Fixed &b) {
+    return (a < b ? a : b);
+}
+
+Fixed& Fixed::max(Fixed &a, Fixed &b) {
+    return (a > b ? a : b);
+}
+
+const Fixed& Fixed::max(const Fixed &a, const Fixed &b) {
+    return (a > b ? a : b);
 }
--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -19,10 +19,29 @@ public:
     Fixed &operator=(const Fixed &other);
     ~Fixed();
 
+    static Fixed&   min(Fixed &a, Fixed &b);
+    static const Fixed&   min(const Fixed &a, const Fixed &b);
+    static Fixed&   max(Fixed &a, Fixed &b);
+    static const Fixed&   max(const Fixed &a, const Fixed &b);
     int getRawBits(void) const;
     void setRawBits(int const raw);
     float toFloat(void) const;
     int toInt(void) const;
+
+    bool operator<(const Fixed &other) const;
+    bool operator>(const Fixed &other) const;
+    bool operator<=(const Fixed &other) const;
+    bool operator>=(const Fixed &other) const;
+    bool operator==(const Fixed &other) const;
+    bool operator!=(const Fixed &other) const;
+    int operator+(const Fixed &other) const;
+    int operator-(const Fixed &other) const;
+    int operator*(const Fixed &other) const;
+    int operator/(const Fixed &other) const;
+    Fixed &operator++();
+    Fixed operator++(int);
+    Fixed &operator--();
+    Fixed operator--(int);
     
 };
 
